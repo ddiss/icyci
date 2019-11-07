0e945a1f1e86e4419bd66035c1180360dd70aad3
David Disseldorp <ddiss@suse.de>
2019-11-07 22:33:37 +0100

    initial commit
    Signed-off-by: David Disseldorp <ddiss@suse.de>

---
+++ icyci.go
@@ -0,0 +1,497 @@
+// Copyright (C) 2019 SUSE LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License version 3 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"time"
+)
+
+func usage() {
+	fmt.Printf("Usage: %s [options]\n", filepath.Base(os.Args[0]))
+	flag.PrintDefaults()
+}
+
+type cliParams struct {
+	sourceUrl     *url.URL
+	sourceBranch  string
+	testScript    string
+	resultsUrl    *url.URL
+	resultsBranch string
+}
+
+type State int
+
+const (
+	uninit State = iota
+	clone
+	verify
+	run
+	save
+	push
+	cleanup
+)
+
+type stateDesc struct {
+	desc    string
+	timeout time.Duration
+}
+
+var states = map[State]stateDesc{
+	uninit:  {"uninitialized", time.Duration(0)},
+	clone:   {"clone source repository", time.Duration(10 * time.Minute)},
+	verify:  {"verify branch HEAD", time.Duration(1 * time.Minute)},
+	run:     {"run test", time.Duration(2 * time.Hour)},
+	save:    {"save test output as git notes", time.Duration(1 * time.Minute)},
+	push:    {"push test output notes", time.Duration(1 * time.Minute)},
+	cleanup: {"cleanup test artifacts", time.Duration(1 * time.Minute)},
+}
+
+const stdoutNotesRef = "refs/notes/icyci.stdout"
+const stderrNotesRef = "refs/notes/icyci.stderr"
+const allNotesGlob = "refs/notes/*"
+
+type cloneCompletion struct {
+	dir string
+	err error
+}
+
+func cloneRepo(ch chan<- cloneCompletion, u *url.URL, branch string,
+	targetDir string) {
+	// TODO branch is not sanitized. Ignore submodules for now.
+	if branch == "" {
+		log.Fatal("empty branch name")
+	}
+	gitArgs := []string{"clone", "--no-checkout", "--single-branch",
+		"--branch", branch, u.String(), targetDir}
+	cmd := exec.Command("git", gitArgs...)
+	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+	err := cmd.Start()
+	if err != nil {
+		log.Fatalf("git failed to start: %v", err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("git %v failed: %v", gitArgs, err)
+	}
+
+	ch <- cloneCompletion{targetDir, nil}
+}
+
+func verifyTag(branch string, tag string) error {
+	log.Printf("GPG verifying tag %s at origin/%s", tag, branch)
+
+	gitArgs := []string{"tag", "--verify", tag}
+	cmd := exec.Command("git", gitArgs...)
+	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	return cmd.Wait()
+}
+
+func verifyCommit(branch string) error {
+	log.Printf("GPG verifying commit at origin/%s HEAD\n", branch)
+
+	gitArgs := []string{"verify-commit", "origin/" + branch}
+	cmd := exec.Command("git", gitArgs...)
+	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	return cmd.Wait()
+}
+
+type verifyCompletion struct {
+	err error
+}
+
+func verifyRepo(ch chan<- verifyCompletion, sourceDir string, branch string) {
+
+	// check for a signed tag at HEAD
+	err := os.Chdir(sourceDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var describeOut bytes.Buffer
+	gitArgs := []string{"describe", "--exact-match", "origin/" + branch}
+	cmd := exec.Command("git", gitArgs...)
+	cmd.Stdout = &describeOut
+	cmd.Stderr = os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalf("git failed to start: %v", err)
+	}
+
+	err = cmd.Wait()
+	if err == nil {
+		// XXX what happens if there are two tags at HEAD?
+		tag := string(describeOut.Bytes())
+		err = verifyTag(branch, tag)
+		if err != nil {
+			log.Fatalf("GPG verification of tag %s at origin/%s failed",
+				tag, branch)
+		}
+	} else {
+		// XXX should ignore merge commits?
+		err = verifyCommit(branch)
+		if err != nil {
+			log.Fatalf("GPG verification of commit at origin/%s failed",
+				branch)
+		}
+	}
+
+	ch <- verifyCompletion{nil}
+}
+
+type runScriptCompletion struct {
+	stdoutFile string
+	stderrFile string
+	err        error
+}
+
+func runScript(ch chan<- runScriptCompletion, workDir string, sourceDir string,
+	branch string, testScript string) {
+
+	err := os.Chdir(sourceDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO probably makes sense to checkout in separate fn
+	gitArgs := []string{"checkout", "--quiet", "origin/" + branch}
+	cmd := exec.Command("git", gitArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalf("git failed to start: %v", err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmpl := runScriptCompletion{}
+
+	cmpl.stdoutFile = path.Join(workDir, "script.stdout")
+	stdoutF, err := os.Create(cmpl.stdoutFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stdoutF.Close()
+
+	cmpl.stderrFile = path.Join(workDir, "script.stderr")
+	stderrF, err := os.Create(cmpl.stderrFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stderrF.Close()
+
+	err = os.Chdir(sourceDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd = exec.Command(testScript)
+	cmd.Stdout = stdoutF
+	cmd.Stderr = stderrF
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalf("git failed to start: %v", err)
+	}
+
+	cmpl.err = cmd.Wait()
+	stdoutF.Sync()
+	stderrF.Sync()
+
+	ch <- cmpl
+}
+
+type notesOut struct {
+	f  string
+	ns string
+}
+
+type addNotesCompletion struct {
+	err error
+}
+
+func addNotes(ch chan<- addNotesCompletion, sourceDir string, branch string,
+	stdoutFile string, stderrFile string) {
+
+	err := os.Chdir(sourceDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, outa := range []notesOut{{ns: stdoutNotesRef, f: stdoutFile},
+		{ns: stderrNotesRef, f: stderrFile}} {
+		gitArgs := []string{"notes", "--ref", outa.ns, "append",
+			"--allow-empty", "-F", outa.f, "origin/" + branch}
+		cmd := exec.Command("git", gitArgs...)
+		cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+		err := cmd.Start()
+		if err != nil {
+			log.Fatalf("git failed to start: %v", err)
+		}
+
+		err = cmd.Wait()
+		if err != nil {
+			log.Fatalf("git %v failed: %v", gitArgs, err)
+		}
+	}
+
+	ch <- addNotesCompletion{nil}
+}
+
+type pushResultsCompletion struct {
+	err error
+}
+
+// push captured stdout and stderr notes to the results repository.
+// The corresponding source branch is *not* pushed, so cloning the results
+// repository only (if different to source) may lead to confusion due to the
+// missing commits referenced by the notes.
+func pushResults(ch chan<- pushResultsCompletion, sourceDir string,
+	branch string, u *url.URL) {
+
+	err := os.Chdir(sourceDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gitArgs := []string{"push", u.String(), allNotesGlob}
+	cmd := exec.Command("git", gitArgs...)
+	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalf("git failed to start: %v", err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("git %v failed: %v", gitArgs, err)
+	}
+
+	ch <- pushResultsCompletion{nil}
+}
+
+type cleanupCompletion struct {
+	err error
+}
+
+func cleanupSource(ch chan<- cleanupCompletion, sourceDir string) {
+
+	err := os.Chdir(sourceDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gitArgs := []string{"clean","-fxd"}
+	cmd := exec.Command("git", gitArgs...)
+	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalf("git failed to start: %v", err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("git %v failed: %v", gitArgs, err)
+	}
+
+	ch <- cleanupCompletion{nil}
+}
+
+func transitionState(newState State, curState *State,
+			stateTransTimer *time.Timer) {
+	log.Printf("transitioning from state %d: %s -> %d: %s\n",
+		*curState, states[*curState].desc,
+		newState, states[newState].desc)
+
+	*curState = newState
+
+	if !stateTransTimer.Stop() {
+		<-stateTransTimer.C
+	}
+	stateTransTimer.Reset(states[newState].timeout)
+}
+
+// event loop to track state of clone, verify, test, push progress
+func eventLoop(params *cliParams, workDir string) {
+
+	sourceDir := path.Join(workDir, "source")
+
+	// TODO reuse channels instead of creating one per activity
+	cloneChan := make(chan cloneCompletion)
+	verifyChan := make(chan verifyCompletion)
+	runScriptChan := make(chan runScriptCompletion)
+	addNotesChan := make(chan addNotesCompletion)
+	pushResultsChan := make(chan pushResultsCompletion)
+	cleanupChan := make(chan cleanupCompletion)
+
+	state := uninit
+	stateTransTimer := time.NewTimer(time.Duration(0))
+	transitionState(clone, &state, stateTransTimer)
+	go func() {
+		cloneRepo(cloneChan, params.sourceUrl, params.sourceBranch,
+			sourceDir)
+	}()
+
+	for {
+		select {
+		case cloneCmpl := <-cloneChan:
+			if cloneCmpl.err != nil {
+				log.Fatal(cloneCmpl.err)
+			}
+			log.Printf("clone completed successfully\n")
+
+			transitionState(verify, &state, stateTransTimer)
+			go func() {
+				verifyRepo(verifyChan, sourceDir,
+					params.sourceBranch)
+			}()
+		case verifyCmpl := <-verifyChan:
+			if verifyCmpl.err != nil {
+				log.Fatal(verifyCmpl.err)
+			}
+			log.Printf("verify completed successfully\n")
+
+			transitionState(run, &state, stateTransTimer)
+			go func() {
+				runScript(runScriptChan, workDir, sourceDir,
+					params.sourceBranch, params.testScript)
+			}()
+		case runScriptCmpl := <-runScriptChan:
+			if runScriptCmpl.err != nil {
+				log.Fatal(runScriptCmpl.err)
+			}
+			log.Printf("test script completed successfully\n")
+
+			transitionState(save, &state, stateTransTimer)
+			go func() {
+				addNotes(addNotesChan, sourceDir,
+					params.sourceBranch,
+					runScriptCmpl.stdoutFile,
+					runScriptCmpl.stderrFile)
+			}()
+		case addNotesCmpl := <-addNotesChan:
+			if addNotesCmpl.err != nil {
+				log.Fatal(addNotesCmpl.err)
+			}
+			log.Printf("git notes completed successfully\n")
+
+			transitionState(push, &state, stateTransTimer)
+			go func() {
+				pushResults(pushResultsChan, sourceDir,
+					params.sourceBranch, params.resultsUrl)
+			}()
+		case pushResultsCmpl := <-pushResultsChan:
+			if pushResultsCmpl.err != nil {
+				log.Fatal(pushResultsCmpl.err)
+			}
+			log.Printf("git push completed successfully\n")
+
+			transitionState(cleanup, &state, stateTransTimer)
+			go func() {
+				cleanupSource(cleanupChan, sourceDir)
+			}()
+		case cleanupCmpl := <-cleanupChan:
+			if cleanupCmpl.err != nil {
+				log.Fatal(cleanupCmpl.err)
+			}
+			log.Printf("cleanup completed successfully\n")
+
+			// all done. TODO: poll for updates
+			return
+		case <-stateTransTimer.C:
+			log.Fatalf("State %v transition timeout!", state)
+		}
+	}
+}
+
+func main() {
+	var sourceRawUrl string
+	var resultsRawUrl string
+	var err error
+
+	params := new(cliParams)
+	flag.Usage = usage
+	flag.StringVar(&sourceRawUrl, "source-repo", "",
+		"Git URL for the repository under test (required)")
+	flag.StringVar(&params.sourceBranch, "source-branch", "master",
+		"Git branch for the repository under test")
+	flag.StringVar(&params.testScript, "test-script", "",
+		"Test script path, relative to source-repo or absolute (required)")
+	flag.StringVar(&resultsRawUrl, "results-repo", "",
+		"Git URL to push test results to (optional)")
+	flag.Parse()
+
+	if sourceRawUrl == "" || params.testScript == "" {
+		usage()
+		return
+	}
+
+	params.sourceUrl, err = url.Parse(sourceRawUrl)
+	if err != nil {
+		log.Fatalf("failed to parse URL \"%s\": %v\n",
+			sourceRawUrl, err)
+	}
+
+	if resultsRawUrl != "" {
+		params.resultsUrl, err = url.Parse(resultsRawUrl)
+		if err != nil {
+			log.Fatalf("failed to parse URL \"%s\": %v\n",
+				resultsRawUrl, err)
+		}
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create a directory to use for staging source, etc.
+	wdir, err := ioutil.TempDir("", "icyci-workspace")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(wdir)
+
+	err = os.Chdir(wdir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Chdir(cwd)
+
+	eventLoop(params, wdir)
+}
---
+++ readme.md
@@ -0,0 +1,74 @@
+icyCI
+=====
+
+icyCI has one job:
+- Pull code from a Git repository
+- Run a build/test/whatever script
+- Push the results to a Git repository
+
+It differs from other CI utilities in a few ways:
+| Lightweight    | Single binary with Git and Go standard library dependencies |
+| Secure         | Only proceeds if branch GPG signature can be verified       |
+| Trustless      | Doesn't need push access to source repositories. Rootless.  |
+| Vendor Neutral | Only relies on standard Git features and protocols          |
+| Fast           | Asynchronous, event based architecture                      |
+| Informative    | Results published as git-notes, viewable from git log       |
+
+
+Usage
+-----
+
+Create a new repository to store your test results. This example uses a local
+directory, but a remote host could also be used.
+```
+> mkdir ~/icyci-results
+> git init --bare ~/icyci-results
+```
+
+Sign a commit at the HEAD of the branch that you wish to test:
+```
+> git commit -S -m "my signed commit"
+> git push <source repo>
+```
+
+Start icyCI, pointing it at your source repo, a build/test script as well as a
+results repository:
+```
+> icyci -source-repo <source repo> -source-branch <my branch> -results-repo ~/icyci-results -test-script <my build/test program>
+```
+
+[wait for icyCI to complete]
+
+Add the results repository as a new remote to your checked out source (where
+you signed the commit).
+```
+> git remote add icyci-results ~/icyci-results
+```
+
+Fetch the icyCI results:
+```
+> git fetch icyci-results  "refs/notes/*:refs/notes/*"
+```
+
+The icyCI test results can now be viewed from your source repository alongside
+the regular git log output, with:
+```
+git log --show-notes="*"
+```
+
+
+Future
+------
+
+- poll for source repository updates (coming soon)
+- improve documentation
+- Support multiple branches within one instance
+- Save and push build/test artifacts, in addition to output
+- periodically push progress while testing
+- Your feature; please raise requests via the issue tracker
+
+
+License
+-------
+
+icyCI is Free Software, published under the AGPLv3 license.
