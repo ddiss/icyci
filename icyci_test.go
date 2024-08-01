ddiss/icyci

commit 80138b5c70c98d89e022f18ea05f1ac3b1196073

icyci: support h/m/s poll-interval suffixes
time.ParseDuration() is already used to parse state timeouts, so it
makes sense to support the helpful h/m/s units for poll-interval too.
For backward compatibility, assume seconds if no duration unit is
provided.

Signed-off-by: David Disseldorp <ddiss@suse.de>

icyci_test.go
@@ -228,7 +228,7 @@ func TestSeparateSrcRslt(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  60,
+		pollInterval:   time.Duration(time.Minute),
 		notesNS:        defNotesNS,
 	}
 
@@ -305,7 +305,7 @@ func TestNewHeadSameSrcRslt(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1, // minimal
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -391,7 +391,7 @@ func TestNewHeadWhileStopped(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1, // minimal
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -521,7 +521,7 @@ func TestStopStart(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1, // minimal
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -644,7 +644,7 @@ func TestSignedTagUnsignedCommit(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1, // minimal
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -725,7 +725,7 @@ func TestMixUnsignedSigned(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1, // minimal
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -958,7 +958,7 @@ func TestMultiInstance(t *testing.T) {
 			testScript:     "./" + i.id + "_test.sh",
 			resultsUrl:     rurl,
 			pushSrcToRslts: false,
-			pollIntervalS:  1,
+			pollInterval:   time.Duration(time.Second),
 			notesNS:        defNotesNS,
 		}
 
@@ -1081,7 +1081,7 @@ func TestScriptEnv(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1, // minimal
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -1145,7 +1145,7 @@ func TestScriptSignalLog(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1, // minimal
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -1216,7 +1216,7 @@ func TestForcePushSrc(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: true,
-		pollIntervalS:  1,
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -1374,7 +1374,7 @@ func TestMultiInstanceSeparateNS(t *testing.T) {
 			testScript:     "./" + i.id + "_test.sh",
 			resultsUrl:     rurl,
 			pushSrcToRslts: false,
-			pollIntervalS:  1,
+			pollInterval:   time.Duration(time.Second),
 			notesNS:        "icyci-" + i.id,
 		}
 
@@ -1547,7 +1547,7 @@ func TestScriptTimeout(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1, // minimal
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -1616,7 +1616,7 @@ func TestScriptExit(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1,
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -1688,7 +1688,7 @@ func TestSrcReference(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rsltsUrl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1,
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -1756,7 +1756,7 @@ func TestMirror(t *testing.T) {
 		sourceBranch:   "mybranch",
 		resultsUrl:     rurl,
 		pushSrcToRslts: true,
-		pollIntervalS:  1,
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -1895,8 +1895,9 @@ func TestCliArgs(t *testing.T) {
 			params.pushSrcToRslts)
 	}
 
-	if params.pollIntervalS != 60 {
-		t.Fatalf("unexpected pollIntervalS %d\n", params.pollIntervalS)
+	// poll-interval without units defaults to seconds
+	if params.pollInterval != time.Duration(60*time.Second) {
+		t.Fatalf("unexpected pollInterval %v\n", params.pollInterval)
 	}
 
 	// https://github.com/ddiss/icyci/issues/4
@@ -1909,14 +1910,76 @@ func TestCliArgs(t *testing.T) {
 		"--push-source-to-results", "true", "--poll-interval", "60",
 	}
 	// reinit for another flag.Parse() call
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
 	var exitedCode *int
 	params = parseCliArgs(func(exitCode int) {
 		exitedCode = new(int)
 		*exitedCode = exitCode
 	})
 	if exitedCode == nil || *exitedCode == 0 {
-		t.Fatalf("unexpected exit request: %v", exitedCode)
+		t.Fatalf("expected exit request: %v", exitedCode)
+	}
+
+	// invalid poll-interval suffix should fail
+	os.Args = []string{"icyci",
+		"--source-repo", "/source.git", "--source-branch", "test",
+		"--test-script", "/test.sh", "--results-repo", "/results.git",
+		"--poll-interval", "60y",
+	}
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	exitedCode = nil
+	params = parseCliArgs(func(exitCode int) {
+		exitedCode = new(int)
+		*exitedCode = exitCode
+	})
+	if exitedCode == nil || *exitedCode == 0 {
+		t.Fatalf("expected exit request: %v", exitedCode)
+	}
+
+	// fancy poll interval
+	os.Args = []string{"icyci",
+		"--source-repo", "/source.git", "--source-branch", "test",
+		"--test-script", "/test.sh", "--results-repo", "/results.git",
+		"--poll-interval", "1h4m",
+	}
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	params = parseCliArgs(func(exitCode int) {
+		t.Fatalf("unexpected exit request: %d", exitCode)
+	})
+	if params.pollInterval != time.Duration(time.Hour+4*time.Minute) {
+		t.Fatalf("unexpected pollInterval %v\n", params.pollInterval)
+	}
+
+	// negative poll interval
+	os.Args = []string{"icyci",
+		"--source-repo", "/source.git", "--source-branch", "test",
+		"--test-script", "/test.sh", "--results-repo", "/results.git",
+		"--poll-interval", "-4h",
+	}
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	exitedCode = nil
+	params = parseCliArgs(func(exitCode int) {
+		exitedCode = new(int)
+		*exitedCode = exitCode
+	})
+	if exitedCode == nil || *exitedCode == 0 {
+		t.Fatalf("expected exit request: %v", exitedCode)
+	}
+
+	// long-form units for poll-interval
+	os.Args = []string{"icyci",
+		"--source-repo", "/source.git", "--source-branch", "test",
+		"--test-script", "/test.sh", "--results-repo", "/results.git",
+		"--poll-interval", "4hours",
+	}
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	exitedCode = nil
+	params = parseCliArgs(func(exitCode int) {
+		exitedCode = new(int)
+		*exitedCode = exitCode
+	})
+	if exitedCode == nil || *exitedCode == 0 {
+		t.Fatalf("expected exit request: %v", exitedCode)
 	}
 }
 
@@ -1942,7 +2005,7 @@ func TestScriptSigterm(t *testing.T) {
 		testScript:     "./src_test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1, // minimal
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -2003,7 +2066,7 @@ func TestBadCmd(t *testing.T) {
 		testScript:     "./does_not_exist.sh", // ENOENT
 		resultsUrl:     srurl,
 		pushSrcToRslts: true,
-		pollIntervalS:  1,
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 	emsg := "./does_not_exist.sh failed: fork/exec ./does_not_exist.sh: " +
@@ -2076,7 +2139,7 @@ func TestNotesDir(t *testing.T) {
 		testScript:     "./t.sh",
 		resultsUrl:     srurl,
 		pushSrcToRslts: true,
-		pollIntervalS:  1,
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -2154,7 +2217,7 @@ func TestStaleNotesDir(t *testing.T) {
 		testScript:     "./t.sh",
 		resultsUrl:     srurl,
 		pushSrcToRslts: true,
-		pollIntervalS:  1,
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -2253,7 +2316,7 @@ func TestSignedMerge(t *testing.T) {
 		testScript:     "./test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1, // minimal
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 
@@ -2324,7 +2387,7 @@ func TestMergeSignedTag(t *testing.T) {
 		testScript:     "./test.sh",
 		resultsUrl:     rurl,
 		pushSrcToRslts: false,
-		pollIntervalS:  1, // minimal
+		pollInterval:   time.Duration(time.Second),
 		notesNS:        defNotesNS,
 	}
 

icyci.go
@@ -47,7 +47,7 @@ type cliParams struct {
 	testScript      string
 	resultsUrl      *url.URL
 	pushSrcToRslts  bool
-	pollIntervalS   uint64
+	pollInterval    time.Duration
 	disableTimeouts bool
 	notesNS         string
 }
@@ -565,8 +565,8 @@ err_out:
 	return err
 }
 
-func pollSource(retCh chan<- error, exitCh chan error, sourceDir string, branch string,
-	pollIntervalS uint64) {
+func pollSource(retCh chan<- error, exitCh chan error, sourceDir string,
+	branch string, pollInterval time.Duration) {
 
 	var err error = nil
 	var pollTimer *time.Timer
@@ -581,7 +581,7 @@ func pollSource(retCh chan<- error, exitCh chan error, sourceDir string, branch 
 	log.Printf("Entering poll loop awaiting new %s commits at %s\n",
 		branch, preFetchRev)
 
-	pollTimer = time.NewTimer(time.Second * time.Duration(pollIntervalS))
+	pollTimer = time.NewTimer(pollInterval)
 	for {
 		select {
 		case err = <-exitCh:
@@ -589,7 +589,7 @@ func pollSource(retCh chan<- error, exitCh chan error, sourceDir string, branch 
 			goto err_out
 
 		case <-pollTimer.C:
-			pollTimer.Reset(time.Second * time.Duration(pollIntervalS))
+			pollTimer.Reset(time.Second * time.Duration(pollInterval))
 
 			err = pollFetch(sourceDir, branch)
 			if err != nil {
@@ -689,7 +689,7 @@ func eventLoop(params *cliParams, workDir string, evSigChan chan os.Signal) {
 							childExitChan,
 							sourceDir,
 							params.sourceBranch,
-							params.pollIntervalS)
+							params.pollInterval)
 					}()
 					continue
 				}
@@ -729,7 +729,7 @@ func eventLoop(params *cliParams, workDir string, evSigChan chan os.Signal) {
 					pollSource(cmplChan, childExitChan,
 						sourceDir,
 						params.sourceBranch,
-						params.pollIntervalS)
+						params.pollInterval)
 				}()
 			case poll:
 				transitionState(verify, &ls)
@@ -749,7 +749,7 @@ func eventLoop(params *cliParams, workDir string, evSigChan chan os.Signal) {
 					pollSource(cmplChan, childExitChan,
 						sourceDir,
 						params.sourceBranch,
-						params.pollIntervalS)
+						params.pollInterval)
 				}()
 				continue
 			}
@@ -855,6 +855,7 @@ func parseCliArgs(exit func(int)) *cliParams {
 	var err error
 
 	params := new(cliParams)
+	params.pollInterval = time.Duration(60 * time.Second)
 	flag.Usage = usage
 	flag.StringVar(&srcRawUrl, "source-repo", "",
 		"Git `URL` for the repository under test (required)")
@@ -870,9 +871,28 @@ func parseCliArgs(exit func(int)) *cliParams {
 	// confusion due to the missing commits referenced by the notes.
 	flag.BoolVar(&params.pushSrcToRslts, "push-source-to-results", true,
 		"Push source-branch and any tag to results-repo, in addition to notes")
-	// TODO should support time.ParseDuration() suffixes, e.g. 1h?
-	flag.Uint64Var(&params.pollIntervalS, "poll-interval", 60,
-		"While idle, poll source-repo for changes at this `seconds` interval")
+	flag.Func("poll-interval",
+		"While idle, poll source-repo for changes at every `duration` "+
+			"(s)econds. (m)inute or (h)our units are also valid, "+
+			"e.g. 1h30m",
+		func(s string) error {
+			// if no suffix then assume seconds for backwards compat
+			if _, err = strconv.ParseUint(s, 10, 64); err == nil {
+				s += "s"
+			}
+			params.pollInterval, err = time.ParseDuration(s)
+			if err != nil {
+				log.Printf("invalid poll-interval \"%s\": %v\n",
+					s, err)
+				exit(1)
+			}
+			if params.pollInterval <= 0 {
+				// TODO: run as single shot if interval is 0
+				err = errors.New("negative or zero poll-interval")
+				exit(1)
+			}
+			return err
+		})
 	flag.BoolVar(&printVers, "v", false, "print version and then exit")
 	flag.BoolVar(&params.disableTimeouts, "disable-timeouts", false,
 		"Disable timeouts for states transitions")

commit bdbf91129777cb0e2ba1c09b4d56475e4d3fbbfd

test: add explicit test for pollTimer.Reset() regression
80138b5 ("icyci: support h/m/s poll-interval suffixes") introduced a
regression in pollSource() where pollTimer.Reset() was provided a bogus
(extremely long) duration. This test explicitly covers the pollSource()
pollTimer reset code-path.

Signed-off-by: David Disseldorp <ddiss@suse.de>

icyci_test.go
@@ -2831,3 +2831,82 @@ func TestSshMixedWithGpg(t *testing.T) {
 		}
 	}
 }
+
+// Explicit regression test for https://github.com/ddiss/icyci/issues/13 where
+// pollTimer.Reset() used a bogus (extremely long) duration.
+// - push unsigned head
+// - start background icyci event loop
+// - sleep 2xpollInterval
+// - push signed head and await notes
+func TestAwaitPollReset(t *testing.T) {
+	tdir := t.TempDir()
+	gpgInit(t, tdir)
+
+	srdir := path.Join(tdir, "test_src_and_rslt")
+	gitReposInit(t, tdir, nil, srdir)
+	gitCmd(t, srdir, "checkout", "-b", "mybranch")
+
+	fileWriteUnsignedCommit(t, srdir, "t.sh", "echo not_tested")
+
+	srurl, err := url.Parse(srdir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	params := cliParams{
+		sourceUrl:      srurl,
+		sourceBranch:   "mybranch",
+		testScript:     "./t.sh",
+		resultsUrl:     srurl,
+		pushSrcToRslts: false,
+		pollInterval:   time.Duration(300 * time.Millisecond),
+		notesNS:        defNotesNS,
+	}
+
+	evSigChan := make(chan os.Signal)
+	exitCmpl := make(chan int)
+	go func() {
+		eventLoop(&params, tdir, evSigChan)
+		exitCmpl <- 1
+	}()
+
+	notesChan := make(chan bytes.Buffer)
+
+	sleepBeforeCommit := 2 * params.pollInterval
+	t.Logf("icyci started with %v pollInterval. Waiting %v for repoll.\n",
+		params.pollInterval, sleepBeforeCommit)
+	waitTimer := time.NewTimer(sleepBeforeCommit)
+	for done := false; !done; {
+		var expected string
+		select {
+		case <-waitTimer.C:
+			if sleepBeforeCommit == 0 {
+				t.Fatal("timeout while waiting for notes or exit")
+			}
+			sleepBeforeCommit = 0
+			c := fileWriteSignedCommit(t, srdir, "t.sh",
+				"echo hi_stdout")
+			go func() {
+				waitNotesLocal(t, srdir, stdoutNotesRef,
+					c, params.pollInterval, notesChan)
+			}()
+			waitTimer.Reset(time.Second * 10)
+		case notes := <-notesChan:
+			if !waitTimer.Stop() {
+				<-waitTimer.C
+			}
+			snotes := string(bytes.TrimRight(notes.Bytes(), "\n"))
+			expected = "hi_stdout"
+			evSigChan <- syscall.SIGTERM
+			if snotes != expected {
+				t.Fatalf("Note %s does not match expected %s\n",
+					snotes, expected)
+			}
+			waitTimer.Reset(time.Second * 10)
+		case <-exitCmpl:
+			if !waitTimer.Stop() {
+				<-waitTimer.C
+			}
+			done = true
+		}
+	}
+}